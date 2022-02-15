45c38f92537b954cb6e3ff07ac061f011ec17211
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-15 17:25:51 +0300

    add two pictures like props

--- src/components/Block3/Block3.tsx
+++ src/components/Block3/Block3.tsx
@@ -2,18 +2,29 @@ import { ReactElement } from 'react';
 
 import style from './Block3.module.css';
 
+import img1 from 'assets/images/react.png';
+import img2 from 'assets/images/redux.jpg';
 import { TitleH3 } from 'common/components';
 import commonStyle from 'common/styles/common.module.css';
-import { Block3Item } from 'components/Block3/Block3Item';
+import { Block3Item, Block3ItemPT } from 'components/Block3/Block3Item';
 
-export const Block3 = (): ReactElement => (
-  <div className={`${commonStyle.generalBlock} ${style.generalBlock}`}>
-    <div className={`${commonStyle.container} ${style.container}`}>
-      <TitleH3 value="Block 3" />
-      <div className={style.picturesBlock}>
-        <Block3Item />
-        <Block3Item />
+export const Block3 = (): ReactElement => {
+  const obj1: Block3ItemPT = {
+    backgroundImage: `url(${img1})`,
+  };
+  const obj2: Block3ItemPT = {
+    backgroundImage: `url(${img2})`,
+  };
+
+  return (
+    <div className={`${commonStyle.generalBlock} ${style.generalBlock}`}>
+      <div className={`${commonStyle.container} ${style.container}`}>
+        <TitleH3 value="Block 3" />
+        <div className={style.picturesBlock}>
+          <Block3Item style={obj2} />
+          <Block3Item style={obj1} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/Block3/Block3Item.tsx
+++ src/components/Block3/Block3Item.tsx
@@ -2,12 +2,20 @@ import { ReactElement } from 'react';
 
 import { TitleH5 } from 'common/components/Title/TitleH5';
 import commonStyle from 'common/styles/common.module.css';
-import style from 'components/Block3/Block3.module.css';
+import style1 from 'components/Block3/Block3.module.css';
 
-export const Block3Item = (): ReactElement => (
-  <div className={style.item}>
-    <div className={style.picture}>picture</div>
-    <div className={style.description}>
+export type Block3ItemPT = {
+  backgroundImage: string;
+};
+
+type NewType = {
+  style: Block3ItemPT;
+};
+
+export const Block3Item = ({ style }: NewType): ReactElement => (
+  <div className={style1.item}>
+    <div className={style1.picture} style={style} />
+    <div className={style1.description}>
       <TitleH5 value="title" />
       <span className={commonStyle.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet asperiores
