7f380de106acb5d3e1861d1128d713c1577172ad
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-07 15:37:46 +0300

    Block 'contacts' is 90%

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -7,14 +7,42 @@ import { Button, Title } from 'common';
 export const Contacts: FC = (): ReactElement => (
   <div className={`${style.generalBlock}`}>
     <div className={`${style.container} `}>
-      <Title value="Contacts title" size="h3" />
+      <Title value="Contacts" size="h3" />
       <div className={style.contact}>
-        <Title value="contact with me" size="h5" />
-        <form>
-          <input type="text" />
-          <input type="text" />
-          <textarea />
-        </form>
+        <Title value="Say me hello !" size="h5" />
+        <div className={style.inputs}>
+          <div className={style.divForm}>
+            <input type="input" placeholder="Name" name="name" id="name" required />
+            <span id="name" className={style.inputLabel}>
+              Name
+            </span>
+          </div>
+          <div className={style.divForm}>
+            <input
+              type="input"
+              placeholder="Contact"
+              name="contact"
+              id="contact"
+              required
+            />
+            <span id="contact" className={style.inputLabel}>
+              Your contact
+            </span>
+          </div>
+          <div className={style.divForm}>
+            <input
+              // style={{ width: '500px' }}
+              type="input"
+              placeholder="Message"
+              name="message"
+              id="message"
+              defaultValue="Hi ! I want to give you a offer"
+            />
+            <span id="message" className={style.inputLabel}>
+              Message
+            </span>
+          </div>
+        </div>
         <Button name="send" />
       </div>
     </div>
