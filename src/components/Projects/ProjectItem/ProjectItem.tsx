511268a371f789f66b5cf3fc5a6764e065b20338
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-07 13:07:20 +0300

    block 'projects' is done

--- src/components/Projects/ProjectItem/ProjectItem.tsx
+++ src/components/Projects/ProjectItem/ProjectItem.tsx
@@ -12,17 +12,20 @@ export type ProjectsType = {
 };
 
 export const ProjectItem: FC<ProjectsType> = ({ img, title }): ReactElement => (
-  <div className={style.item}>
-    <div className={style.picture} style={img}>
-      <Button name="look" />
-    </div>
-    <div className={style.description}>
-      <Title value={title} size="h5" />
-      <span className={style.text}>
-        Stack: SCSS, JavaScript, TypeScript, React, Redux, Reduxthunk, Redux-form, Axios,
-        Reselect; Using TypeScript for best code control; Reusable components development;
-        Refactoring, bug fixing, deploying; Organizing correct app architecture
-      </span>
+  <div className={style.itemShadow}>
+    <div className={style.item}>
+      <div className={style.picture} style={img}>
+        <Button name="look" />
+      </div>
+      <div className={style.description}>
+        <Title value={title} size="h5" />
+        <span className={style.text}>
+          Stack: SCSS, JavaScript, TypeScript, React, Redux, Reduxthunk, Redux-form,
+          Axios, Reselect; Using TypeScript for best code control; Reusable components
+          development; Refactoring, bug fixing, deploying; Organizing correct app
+          architecture
+        </span>
+      </div>
     </div>
   </div>
 );
