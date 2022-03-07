e6312eff16b156c6672cc7a5ae22b24a17d20971
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-07 15:59:43 +0300

    Block 'footer' is 90%

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,25 @@
 import { FC, ReactElement } from 'react';
 
 import style from './Footer.module.scss';
+import CODEWARS from './icons/codewars.png';
+import GIT from './icons/github.png';
+import LINK from './icons/linkedin.png';
+import TELEGRAM from './icons/telegram.png';
 
 import { Title } from 'common';
+
+const arr = [CODEWARS, GIT, LINK, TELEGRAM];
 
 export const Footer: FC = (): ReactElement => (
   <div className={style.generalBlock}>
     <div className={style.container}>
       <Title value="Footer" size="h3" />
       <div className={style.cubesBlock}>
-        <div className={style.cube} />
-        <div className={style.cube} />
-        <div className={style.cube} />
-        <div className={style.cube} />
+        {arr.map(el => (
+          <div key={el} className={style.cube}>
+            <div className={style.icon} style={{ backgroundImage: `url(${el})` }} />
+          </div>
+        ))}
       </div>
       <Title value="© 2022 Copyrights. All Rights Reserved." size="h5" />
     </div>
