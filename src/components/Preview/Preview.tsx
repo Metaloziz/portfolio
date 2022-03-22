f7a05342060e7ccf8738c9b5553d6ab4ca6abb53
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-22 13:39:46 +0300

    refactoring preview

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactElement } from 'react';
 
+import Particles from 'react-tsparticles';
+
 import style from './Preview.module.scss';
 
 import img1 from 'assets/images/ava.jpg';
@@ -13,8 +15,21 @@ const obj1: BackgroundImage = {
   backgroundImage: `url(${img1})`,
 };
 
+const particleParams = {
+  particles: {
+    number: {
+      value: 15,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
 export const Preview: FC = (): ReactElement => (
   <div className={style.generalBlock}>
+    <Particles className={style.particles} params={particleParams} />
     <div className={style.container}>
       <div className={style.photo} style={obj1} />
       <div className={style.text}>
