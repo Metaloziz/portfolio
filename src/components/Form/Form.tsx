44f30d370b3d625d4892c327f3c7f217506480b7
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-04-03 16:17:50 +0300

    add react-hook-form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,54 +1,83 @@
 import { FC, ReactElement } from 'react';
 
-import { Fade } from 'react-awesome-reveal';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Button, Title } from 'common';
 import { Slider } from 'common/components/Slider/Slider';
 import style from 'components/Form/Form.module.scss';
 
-export const Form: FC = (): ReactElement => (
-  <div id="contacts" className={`${style.generalBlock}`}>
-    <div className={`${style.container} `}>
-      <Slider>
-        <Title value="Say me hello !" size="h3" />
-      </Slider>
-      <div className={style.contact}>
-        <div className={style.inputs}>
-          <Fade triggerOnce>
-            <div className={style.divForm}>
-              <input type="input" placeholder="Name" name="name" id="name" required />
-              <span id="name" className={style.inputLabel}>
-                Your name
-              </span>
+type FormDataType = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+export const Form: FC = (): ReactElement => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormDataType>();
+
+  const onLoginClick: SubmitHandler<FormDataType> = (e): void => {
+    console.log(e);
+  };
+
+  return (
+    <div id="contacts" className={`${style.generalBlock}`}>
+      <div className={`${style.container} `}>
+        <Slider>
+          <Title value="Say me hello !" size="h3" />
+        </Slider>
+
+        <div className={style.form}>
+          <form onSubmit={handleSubmit(onLoginClick)}>
+            <div className={style.inputs}>
+              {/* <Fade triggerOnce> */}
+              <div className={style.divForm}>
+                <label htmlFor="name">
+                  <input
+                    id="name"
+                    placeholder="name"
+                    {...register('name', { required: true })}
+                  />
+
+                  <span id="name" className={style.inputLabel}>
+                    Your name
+                  </span>
+                </label>
+              </div>
+              <div className={style.divForm}>
+                <input
+                  id="email"
+                  placeholder="email"
+                  {...register('email', { required: true })}
+                />
+
+                <span id="email" className={style.inputLabel}>
+                  Your email
+                </span>
+              </div>
+              <div className={style.divForm}>
+                <input
+                  id="message"
+                  placeholder="message"
+                  {...register('message', { required: true })}
+                />
+                <span id="message" className={style.inputLabel}>
+                  Message
+                </span>
+              </div>
+              {/* </Fade> */}
             </div>
-            <div className={style.divForm}>
-              <input
-                type="input"
-                placeholder="Contact"
-                name="contact"
-                id="contact"
-                required
-              />
-              <span id="contact" className={style.inputLabel}>
-                Your contact
-              </span>
+            <Button name="send" type="submit" />
+            <div>
+              {errors.name?.type === 'required' && 'name is required'}
+              {errors.email?.type === 'required' && 'email is required'}
             </div>
-            <div className={style.divForm}>
-              <input
-                type="input"
-                placeholder="Message"
-                name="message"
-                id="message"
-                defaultValue="Hi ! I have a offer for you "
-              />
-              <span id="message" className={style.inputLabel}>
-                Message
-              </span>
-            </div>
-          </Fade>
+          </form>
         </div>
-        <Button name="send" />
       </div>
     </div>
-  </div>
-);
+  );
+};
