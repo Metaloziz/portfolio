2b2abeb94138a42ca95ea312369e31bb69d9ae2b
Andrew Gaityukevich <AndrewGaity@yandex.by>
2023-04-10 12:32:15 +0300

    feet: added skills

--- src/components/Skills/data/data.ts
+++ src/components/Skills/data/data.ts
@@ -5,7 +5,9 @@ import GITHUB from 'assets/images/icons_skills/github.png';
 import HTML5 from 'assets/images/icons_skills/html.png';
 import JS from 'assets/images/icons_skills/js.png';
 import MATERIAL_UI from 'assets/images/icons_skills/material_ui.png';
+import MOBX from 'assets/images/icons_skills/MOBX.svg';
 import REACT from 'assets/images/icons_skills/react.png';
+import REACT_HOOK_FORM from 'assets/images/icons_skills/Reacthook form.png';
 import REDUX from 'assets/images/icons_skills/redux.png';
 import SASS from 'assets/images/icons_skills/sass.png';
 import STORYBOOK from 'assets/images/icons_skills/storybook.png';
@@ -25,12 +27,14 @@ export const arrSkills: SkillsType = [
   { title: 'Type Script', img: { backgroundImage: `url(${TS})` } },
   { title: 'REACT', img: { backgroundImage: `url(${REACT})` } },
   { title: 'REDUX', img: { backgroundImage: `url(${REDUX})` } },
+  { title: 'MOBX', img: { backgroundImage: `url(${MOBX})` } },
   { title: 'HTML5', img: { backgroundImage: `url(${HTML5})` } },
   { title: 'CSS3', img: { backgroundImage: `url(${CSS})` } },
+  { title: 'SASS', img: { backgroundImage: `url(${SASS})` } },
   { title: 'FORMIK', img: { backgroundImage: `url(${FORMIK})` } },
   { title: 'GITHUB', img: { backgroundImage: `url(${GITHUB})` } },
   { title: 'AXIOS', img: { backgroundImage: `url(${AXIOS})` } },
-  { title: 'SASS', img: { backgroundImage: `url(${SASS})` } },
   { title: 'STORYBOOK', img: { backgroundImage: `url(${STORYBOOK})` } },
   { title: 'MATERIAL UI', img: { backgroundImage: `url(${MATERIAL_UI})` } },
+  { title: 'React Hook Form', img: { backgroundImage: `url(${REACT_HOOK_FORM})` } },
 ];
